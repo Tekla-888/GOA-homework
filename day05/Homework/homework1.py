a44f41c1d422f52213d22c5afbe9cf6d6005e266
Tekla-888 <teklashekiladze5@gmail.com>
2024-10-19 15:04:34 +0400

    commit

---
+++ day05/Homework/homework1.py
@@ -0,0 +1,5 @@
+celsius=30
+
+fahrenheit =(0*9/5)+32
+
+print(celsius*fahrenheit)
---
+++ day05/Homework/homework2.py
@@ -0,0 +1,11 @@
+mumage=33
+dadage=36
+sisterage1=13
+sisterage2=3
+sisterage3=1
+
+print(mumage+20)
+print(dadage+20)
+print(sisterage1+20)
+print(sisterage2+20)
+print(sisterage3+20)
