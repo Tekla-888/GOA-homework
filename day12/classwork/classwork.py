38db8e9ace4d99273dd6ac24dadbaf29879241e6
Tekla-888 <teklashekiladze5@gmail.com>
2024-10-30 00:14:24 +0400

    commit

---
+++ day12/classwork/classwork.py
@@ -0,0 +1,24 @@
+name=input("enter your name:" )
+print(name)
+print(type(name))
+
+
+surname=input("enter your surname:")
+print(surname)
+print(type(surename))
+
+
+
+age=input("enter your age:")
+print(age)
+print(type(age))
+
+
+num1=input("enter number1")
+print(num1)
+print(type(num1))
+
+
+num2=input("enter number2 :")
+print(num2)
+print(type(num2))
