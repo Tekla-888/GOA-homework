23ab4c2a42d8602368393f16fc4357d0a1be92dd
Tekla-888 <teklashekiladze5@gmail.com>
2025-03-29 18:30:06 +0400

    commit

---
+++ day48/homework/hw.py
@@ -0,0 +1,15 @@
+#https://www.codewars.com/kata/56b1f01c247c01db92000076
+#https://www.codewars.com/kata/557cd6882bfa3c8a9f0000c1
+#https://www.codewars.com/kata/5aa736a455f906981800360d
+#https://www.codewars.com/kata/5a2be17aee1aaefe2a000151
+#https://www.codewars.com/kata/55cbc3586671f6aa070000fb
+def double_char(s):
+    
+    result = ""
+    
+    for char in s:
+        
+        result += char * 2
+        
+        
+        return result
